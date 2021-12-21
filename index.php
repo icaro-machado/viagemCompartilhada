6434c2130d93f3a091d9c4d1d6c19d687d2a6138
icaro-machado <71112037+icaro-machado@users.noreply.github.com>
2021-12-21 00:37:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ require_once "Viajantes.php";
 
 $participantesDaViagem = new Viajantes(["Icaro","Rafa","Maria","Tiao"]);
 
-var_dump($participantesDaViagem);
-
 $i = 0;
 $numeroDeViajantes = count($participantesDaViagem->viajantes);
 
@@ -18,14 +16,22 @@ while ($i < $numeroDeViajantes) {
     echo "-" . $participantesDaViagem->viajantes[$i] . PHP_EOL;
     $i++;
 }
-
 echo PHP_EOL;
  
             // Finalização da inclusão dos participantes da Viagem
 
             // Início da Inclusão de novas compras
 
-$compra1 = new Compra("Pizzaria",10,["Icaro","Rafa"]);
+$compra1 = new Compra("Pizzaria",10,["Icaro", "Rafa"]);
 
+$numeroDeParticipantes = count($compra1->participantesDaCompra);
+$comparacaoNumeroDeParticipantesComViajantes = count(array_intersect($compra1->participantesDaCompra, $participantesDaViagem->viajantes));
 
-var_dump($compra1);
+if ($numeroDeParticipantes == $comparacaoNumeroDeParticipantesComViajantes && $numeroDeParticipantes != 0):
+    $valorParaCada = $compra1->valor/$numeroDeParticipantes;
+    echo "Todos os participantes dessa compra estão na viagem!" . PHP_EOL;
+    echo "O valor para cada participante dessa compra será de R$" . $valorParaCada . PHP_EOL;
+else:
+    echo "Os participantes dessa compra não estão na viagem!" . PHP_EOL;
+    echo "Essa compra não é válida!" . PHP_EOL;
+endif;
