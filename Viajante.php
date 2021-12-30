c2fad09eab1e266f9b22f4f00a55931909783b74
Icaro Machado <icarodesousamachado@gmail.com>
2021-12-29 21:39:40 -0300

    Atualizacao

--- Viajante.php
+++ Viajante.php
@@ -4,10 +4,17 @@ class Viajante
 {
     public $viajante;
     public $saldo;
+    public $valorDividido;
 
     public function __construct(string $viajante, float $saldo = 0)
     {
         $this->viajante = $viajante;
         $this->saldo = $saldo;
+    }
+
+    public function atualizarSaldo(float $valorDividido)
+    {        
+        $saldo = $saldo + $valorDividido;
+        return $saldo;
     }
 }
--- index.php
+++ index.php
@@ -20,7 +20,7 @@ echo "Os participantes dessa viagem são:" . PHP_EOL;
 while ($i < $numeroDeViajantes) {
     echo "- " . $participantesDaViagem[$i]->viajante . PHP_EOL;
     $i++;
-}
+};
 
 echo PHP_EOL;
 
@@ -28,23 +28,45 @@ echo PHP_EOL;
 
             // Início da Inclusão de novas compras
 
-$primeiraCompra = new Compra(
+$compras = [
+    $primeiraCompra = new Compra(
     "Pizzaria",
     60,
     [$participantesDaViagem[0]->viajante, $participantesDaViagem[1]->viajante],
     [$participantesDaViagem[0]->viajante]
-);
+    ),
 
-$segundaCompra = new Compra(
+    $segundaCompra = new Compra(
     "Shopping",
     350,
     [$participantesDaViagem[0]->viajante, $participantesDaViagem[1]->viajante, $participantesDaViagem[2]->viajante],
     [$participantesDaViagem[2]->viajante]
-);
+    ),
 
-$terceiraCompra = new Compra(
+    $terceiraCompra = new Compra(
     "Passeio do roteiro",
     450,
     [$participantesDaViagem[0]->viajante, $participantesDaViagem[1]->viajante, $participantesDaViagem[3]->viajante],
     [$participantesDaViagem[0]->viajante]
-);
+    ),
+
+    $quartaCompra = new Compra(
+    "Roupas",
+    350,
+    [$participantesDaViagem[0]->viajante, $participantesDaViagem[3]->viajante],
+    [$participantesDaViagem[0]->viajante]
+    )
+];
+
+$z = 0;
+$numeroDeCompras = count($compras);
+
+echo "As compras realizadas foram:" . PHP_EOL;
+
+while ($z < $numeroDeCompras) {
+    echo "- " . $compras[$z]->nomeDaCompra . PHP_EOL;
+    echo "  - Valor: R$" . $compras[$z]->valor . PHP_EOL;
+    echo "  - Pagador: " . $compras[$z]->pagador[0] . PHP_EOL;
+    echo "  - Essa compra teve " . count($compras[$z]->participantesDaCompra) . " participantes." . PHP_EOL;
+    $z++;
+};
